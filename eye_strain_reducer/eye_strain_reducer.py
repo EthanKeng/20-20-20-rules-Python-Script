8c44b8bbb02b354fc4e680007ed2cad63fab1055
YK <36765774+EthanKeng@users.noreply.github.com>
2022-01-27 00:28:57 +0900

    added time prompt message and intro mp3

--- eye_strain_reducer/eye_strain_reducer.py
+++ eye_strain_reducer/eye_strain_reducer.py
@@ -18,10 +18,13 @@ def turnoff():
     if ( (check_audio == 'ON')) | (check_audio != 'ON'):
          print (strftime("%H:%M:%S", localtime()) + "--> OKAY!!")
          pygame.init()
+         pygame.mixer.music.load('take_a_rest.mp3')
+         pygame.mixer.music.play(0)
          screen = pygame.mouse.set_visible(0)
          screen = pygame.display.set_mode((size),pygame.FULLSCREEN)
          time.sleep( 20 )
          print (strftime("%H:%M:%S", localtime()) + "--> Starting...again")
+         
          pygame.mixer.music.load('notification.mp3')
          pygame.mixer.music.play(0)
          while pygame.mixer.music.get_busy(): 
@@ -32,7 +35,10 @@ def turnoff():
 
 schedule.every(duration).seconds.do(turnoff)
 print (strftime("%H:%M:%S", localtime()) + "--> Starting...")
-
-while 1:
+t=0
+while True:
     schedule.run_pending()
-    time.sleep(1)
+    time.sleep(0.5)
+    t=t+1
+    if t%120==0:
+      print(strftime("%H:%M:%S", localtime())+"-->Still running,", t/120,"minutes")
